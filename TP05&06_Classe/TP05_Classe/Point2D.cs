dd8b6856eedb71ddf2d7a08557c562d3d296313f
coolwahoo <coolwahoo@yahoo.fr>
2018-04-03 14:16:46 +0200

    Add files via upload

---
+++ TP05&06_Classe/TP05_Classe/Point2D.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05_Classe
+{
+    class Point2D
+    {
+        public Point2D() : this(0, 0)
+        {
+
+        }
+
+        public Point2D(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        private int x; //Champs en .Net équivalent à Atrribut en Java
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y { get; set; }
+
+
+        //int x, y;
+
+        //public void SetX(int dx)
+        //{
+        //    x = dx;
+        //}
+
+        //public void SetY(int dy)
+        //{
+        //    y = dy;
+        //}
+
+        //public int GetX() { return this.x; }
+
+        //public int GetY() { return this.y; }
+
+        public string Afficher()
+        {
+            return string.Format("Valeur du point 2D : [{0},{1}]", x, Y);
+        }
+
+        public void Translater(int dx, int dy)
+        {
+
+            this.x += dx;
+            this.Y += dy;
+        }
+    }
+}
---
+++ TP05&06_Classe/TP05_Classe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Création d'un premier Point 2D");
+            Point2D p1 = new Point2D();
+            //p1.SetX(10);
+            //p1.SetY(20);
+            p1.X = 10;
+            p1.Y = 20;
+
+            Console.WriteLine("Affichage de p1: {0}", p1.Afficher());
+
+            Console.WriteLine("Création d'un deuxième Point 2D");
+            Point2D p2 = new Point2D();
+            p2.X = 3;
+            p2.Y = 10;
+            //p2.SetX( 3);
+            //p2.SetY(10);
+
+            Console.WriteLine("Affichage de p2: {0}", p2.Afficher());
+            Console.WriteLine("Translation 15,20");
+            p2.Translater(15, 20);
+            Console.WriteLine("Affichage de p2: {0}", p2.Afficher());
+
+            Console.WriteLine("Création d'un troisième Point 2D");
+            Point2D p3 = new Point2D();
+            Console.WriteLine("Affichage de p3: {0}", p3.Afficher());
+
+            Point2D p4 = new Point2D(1, 1);
+            Console.WriteLine("Affichage de p4: {0}", p4.Afficher());
+
+            Point2D p5 = new Point2D { X = 10};
+            Console.WriteLine("Affichage de p5: {0}", p5.Afficher());
+
+            Console.WriteLine(p5.ToString());
+            Console.ReadLine();
+        }
+    }
+}
